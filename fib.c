fbb1234c6835575f52d8dbc076a4e7e9f5b07545
mohsentux <mohsentux@live.com>
2020-11-09 03:31:27 +0330

    Update_1

--- fib.c
+++ fib.c
@@ -5,7 +5,7 @@
 #include <inttypes.h>
 
 // Recursive function call
-int_fast64_t fibonacci(int_fast64_t n) {
+int64_t fibonacci(int64_t n) {
     if (n == 0)
         return 0;
     else if (n == 1)
@@ -14,10 +14,16 @@ int_fast64_t fibonacci(int_fast64_t n) {
         return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+// Tail Recursive function call
+int64_t tail_fibonacci(int64_t n, int64_t value, int64_t previous) {
+    if(n == 0) return previous;
+    return tail_fibonacci(n - 1, value+previous, value);
+}
+
 int main() {
     printf("Enter the number of fibonacci you want: ");
     int_fast64_t fib;
     scanf("%lld", &fib);
-    printf("fibonacci %lld equals %lld", fib, fibonacci(fib));
+    printf("fibonacci %lld equals %lld", fib, tail_fibonacci(fib, 1, 0));
     return 0;
 }
